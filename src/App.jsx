09476fa52c16ef03da53852a7419162919d9e6cb
Ambre <ambre.morard@students.hevs.ch>
2026-06-23 14:29:22 +0200

    refactor: extract header into Header component

--- src/App.jsx
+++ src/App.jsx
@@ -2,30 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import PageTemplate from './components/PageTemplate';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <Router>
       <div className="app-container">
-        
-        {/* HEADER & NAVIGATION (Hardcoded in React) */}
-        <header>
-          <img src="/ressources/images/devil-duck_icon_cropped.jpg" alt="Duck icon" />
-          <h1>HES-SO Vs - 64-31 - Web Development</h1>
-          <nav>
-            <ul>
-
-              <li><Link to="/description">Description</Link></li>
-              <li><Link to="/mockup">Game Mockup</Link></li>
-              <li><Link to="/maquettes">Duck Mockup</Link></li>
-              <li><Link to="/flux">Flux</Link></li>
-              <li><Link to="/result">Results</Link></li>
-              <li><Link to="/detailed-game-description">Skills Overview</Link></li>
-              <li><Link to="/journal-de-bord">Logbook</Link></li>
-              <li><Link to="/links">Links</Link></li>
-            </ul>
-          </nav>
-        </header>
+        <Header />
 
         {/* DYNAMIC CONTENT (Fetched from WordPress depending on the URL) */}
         <main>
---
+++ src/components/Header.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
+
+function Header() {
+    return (
+        <header>
+            {/* HEADER & NAVIGATION */}
+            <img src="/ressources/images/devil-duck_icon_cropped.jpg" alt="Duck icon" />
+            <h1>HES-SO Vs - 64-31 - Web Development</h1>
+            <nav>
+                <ul>
+                    <li><Link to="/description">Description</Link></li>
+                    <li><Link to="/mockup">Game Mockup</Link></li>
+                    <li><Link to="/maquettes">Duck Mockup</Link></li>
+                    <li><Link to="/flux">Flux</Link></li>
+                    <li><Link to="/result">Results</Link></li>
+                    <li><Link to="/detailed-game-description">Skills Overview</Link></li>
+                    <li><Link to="/journal-de-bord">Logbook</Link></li>
+                    <li><Link to="/links">Links</Link></li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
